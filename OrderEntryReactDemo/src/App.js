2bd4884efab792c72911313900516ae9d1679448
namanchopra21087 <namanchopra21087@gmail.com>
2021-03-04 20:17:26 +0530

    react app with UAL added

---
+++ OrderEntryReactDemo/src/App.js
@@ -0,0 +1,35 @@
+/*
+  Order Entry React Demo for EOSIO Training & Certification: AD101
+
+  Import and implement UAL plugins, consumer, and wrapper in this file
+*/
+
+import React from 'react';
+import { JsonRpc } from 'eosjs'
+import OrderEntryApp from './components/orderentry';
+
+import { UALProvider, withUAL } from 'ual-reactjs-renderer';
+import {Anchor} from 'ual-anchor';
+function App() {
+  
+  const ourNetwork={
+    chainId:"cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f",
+    rpcEndpoints:[{protocol:"http",host:"localhost",port:8888}]
+  };
+  const anchor=new Anchor([ourNetwork],{
+    appName:'OrderEntryApp',
+    rpc:new JsonRpc('${ourNetwork.rpcEndpoints.protocol},${ourNetwork.rpcEndpoints.host},${ourNetwork.rpcEndpoints.port}'),
+    service:"https://cb.anchor.link",
+    disableGreymassFuel:false,
+    requestStatus:false
+  })
+  const OrderEntryAppConsumer=withUAL(OrderEntryApp);
+  OrderEntryAppConsumer.displayName="OrderEntryApp";
+  return(
+    <UALProvider chains={[ourNetwork]} authenticators={[anchor]} appName={'OrderEntryApp'}>
+        <OrderEntryAppConsumer/>
+    </UALProvider>
+  )
+}
+
+export default App;
